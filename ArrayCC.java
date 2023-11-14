1d96b8f5e66424ca3a5dcc93e74e3dd38448abb5
Khadkashankar <shankar.755421@snsc.tu.edu.np>
2023-11-14 21:42:29 +0545

    max subarray sum

--- ArrayCC.java
+++ ArrayCC.java
@@ -84,6 +84,28 @@ public class ArrayCC {
         System.out.println(total_pairs);
     }
 
+    public static void printSubarrayMaxSum(int numbers[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                currSum = 0;
+                for (int k = start; k <= end; k++) {
+                    //subarray sum
+                    currSum += numbers[k];
+                }
+                        System.out.println(currSum);
+                        if (maxSum < currSum) {
+                            maxSum = currSum;
+               }
+            }
+           
+        }  
+        System.out.println("max sum = "+maxSum);
+    }
+
     public static void main(String[] args) {
         int numbers[] = { 10, 12, 13, 8, 100, 48 };
             // int key = 10;
@@ -110,7 +132,10 @@ public class ArrayCC {
                 // printPairs(numbers);
 
                 //print subarrays
-                printSubarray(numbers);
+                //printSubarray(numbers);
+
+                //max subarray sum
+                printSubarrayMaxSum(numbers);
                 }
     }
 
