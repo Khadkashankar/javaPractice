7768ccaccc1a724c55be1552ee89f30815e7d930
Khadkashankar <shankar.755421@snsc.tu.edu.np>
2023-12-12 23:03:41 +0545

    counting sort

--- Sorting.java
+++ Sorting.java
@@ -43,11 +43,32 @@ public class Sorting {
         }
     }
 
+    public static void CountigSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        //sorting
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;                
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = { 4, 8, 3, 9, 5, 7 };
         // BubbleSort(arr);
         // SelectionSort(arr);
-        InsertionSort(arr);
+        // InsertionSort(arr);
+        CountigSort(arr);
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
